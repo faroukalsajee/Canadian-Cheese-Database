cbf4bc4ec5754cd74139194ec9e573b2c7100aea
faroukalsajee <faroukalsajee@gmail.com>
2021-07-26 11:25:54 -0700

    resume ajax request arguments

--- script.js
+++ script.js
@@ -10,5 +10,16 @@ $.ajax({
   data: { getdata: "https://od-do.agr.gc.ca/canadianCheeseDirectory.json" },
   success: function (response) {
     console.log(response);
-  },
-});
+  
+
+$.each(response.CheeseDirectory, function (index, value) {
+  var organic = "Yes";
+  if (value.Organic == 0) organic = "No";
+
+  var name = value.CheeseNameEn;
+
+  if (default_lang == "fr") {
+    if (value.CheeseNameFr !== "") {
+      name = value.CheeseNameFr;
+    }
+  }
